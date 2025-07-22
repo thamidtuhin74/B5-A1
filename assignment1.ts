d9ee3f6bc5ecf8e05f53024bbef6ba15279feab9
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2025-07-22 22:50:39 +0600

    prblem 3 solved

--- assignment1.ts
+++ assignment1.ts
@@ -36,4 +36,13 @@
 
     console.log(filterByRating(books));
   }
+
+  {
+    function concatenateArrays<T>(...arrays: T[][]): T[] {
+      const newArray: T[] = arrays.flat();
+      return newArray;
+    }
+    console.log(concatenateArrays(["a", "b"], ["c"]));
+    console.log(concatenateArrays([1, 2], [3, 4], [5]));
+  }
 }
