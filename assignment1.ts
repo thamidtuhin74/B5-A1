85951a9e115c9d038f2bc0489024e44c8f9a5a53
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2025-07-23 04:06:08 +0600

    problem 4 solved

--- assignment1.ts
+++ assignment1.ts
@@ -45,4 +45,37 @@
     console.log(concatenateArrays(["a", "b"], ["c"]));
     console.log(concatenateArrays([1, 2], [3, 4], [5]));
   }
+
+  {
+    class Vehicle {
+      private make: string;
+      private year: number;
+
+      constructor(make: string, year: number){
+        this.make = make;
+        this.year =  year
+      }
+
+      getInfo(){
+        console.log(`"Make: ${this.make}, Year: ${this.year}"`)
+      }
+    }
+
+    class Car extends Vehicle{
+      private model: string;
+      constructor(make: string, year: number, model: string){
+        super(make, year);
+        this.model = model;
+      }
+      getModel(){
+        console.log(`"Model: ${this.model}"`)
+      }
+    }
+
+    const mycar = new Car("Toyota", 2020, "Corolla");
+    mycar.getInfo();
+    mycar.getModel()
+
+
+  }
 }
