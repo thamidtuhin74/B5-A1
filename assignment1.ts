6b8556b51df332d3e168d46c750cda26ce6a9702
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2025-07-22 22:36:53 +0600

    prblem 2 solved

--- assignment1.ts
+++ assignment1.ts
@@ -13,4 +13,27 @@
 
     console.log(formatString("Tuhin", false));
   }
+
+  {
+    function filterByRating(
+      items: { title: string; rating: number }[]
+    ): { title: string; rating: number }[] {
+      //   return [{ title: "string", rating: 5 }];
+      let filterdItem: { title: string; rating: number }[] = [];
+      items.map((item) => {
+        if (item.rating >= 4) {
+          filterdItem.push(item);
+        }
+      });
+      return filterdItem;
+    }
+
+    const books = [
+      { title: "Book A", rating: 4.5 },
+      { title: "Book B", rating: 3.2 },
+      { title: "Book C", rating: 5.0 },
+    ];
+
+    console.log(filterByRating(books));
+  }
 }
