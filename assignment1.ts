de6f67d53839e985f803fc9bf7363e4a99f6349c
thamidtuhin74 <altahmiduzzamantuhin74@gmail.com>
2025-07-25 21:28:12 +0600

    slv 7

--- assignment1.ts
+++ assignment1.ts
@@ -133,4 +133,19 @@
 
     console.log(getMostExpensiveProduct(products));
   }
+  {
+    enum Day {
+      Monday = "Weekday",
+      Tuesday = "Weekday",
+      Wednesday = "Weekday",
+      Thursday = "Weekday",
+      Friday = "Weekday",
+      Saturday = "Weekday",
+      Sunday = "Weekend",
+    }
+    function getDayType(day: Day): string {
+      return day;
+    }
+    console.log(getDayType(Day.Sunday));
+  }
 }
